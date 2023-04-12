ce2f934886aca3f874efb4bf792d2bf97d6499a6
Lucas Moreira <lucasmsea@outlook.com>
2023-04-12 12:06:34 -0300

    infra: add ingress controller configurations

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,16 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+app.get("/api/users/", (req, res) => {
+  res.json({
+    message: "✅ Server is up and running!"
+  });
+});
+
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("hey there");
+});
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
